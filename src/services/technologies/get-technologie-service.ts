ccefa0ab23932540d2fa7876fcbc5b283da9b8ec
Renan Lira <renanalmeida2015.ra@gmail.com>
2025-04-08 21:14:53 -0300

    :bug: corrige a URL da API no serviço GetTechnologieService para incluir o prefixo 'api'

--- src/services/technologies/get-technologie-service.ts
+++ src/services/technologies/get-technologie-service.ts
@@ -1,6 +1,6 @@
 
 export async function GetTechnologieService(id: string) {
-    const res = await fetch(`http://localhost:3000/technologies/${id}`, {
+    const res = await fetch(`http://localhost:3000/api/technologies/${id}`, {
         next: {
             revalidate: 60,
         },
